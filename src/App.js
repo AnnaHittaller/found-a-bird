00e3796fddd4d7f5655ccb6d9eb251c1dca19b94
Anna Hittaller <hittaller.anna@gmail.com>
2023-06-10 13:15:03 +0200

    minor mobile view changes

--- src/App.js
+++ src/App.js
@@ -4,38 +4,41 @@ import "./generalStyles.css";
 import AllRoutes from "./AllRoutes";
 
 export default function App() {
-  const location = useLocation();
+	const location = useLocation();
 
-  return (
-    <>
-      <div
-        className="desktop-img"
-        style={{
-          width:
-            location.pathname === "/" || location.pathname === "/gallery"
-              ? "0vw"
-              : "",
-        }}
-      ></div>
-      {location.pathname === "/" ? null : (
-        <span className="desktop-img-span">Photo by Hans Veth on Unsplash</span>
-      )}
-      <div
-        className="App"
-        style={{
-          backgroundImage:
-            location.pathname === "/"
-              ? "url('https://images.unsplash.com/photo-1605197585663-011e30915ab1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
-              : null,
-          backgroundColor: location.pathname === "/" ? null : "#fffde0",
-          width:
-            location.pathname === "/" || location.pathname === "/gallery"
-              ? "100vw"
-              : "",
-        }}
-      >
-        <AllRoutes />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div
+				className="desktop-img"
+				style={{
+					width:
+						location.pathname === "/" || location.pathname === "/gallery"
+							? "0vw"
+							: "",
+				}}></div>
+			{location.pathname === "/" ? null : (
+				<span className="desktop-img-span">Photo by Hans Veth on Unsplash</span>
+			)}
+			<div
+				className="App"
+				style={{
+					backgroundImage:
+						location.pathname === "/"
+							? "url('https://images.unsplash.com/photo-1605197585663-011e30915ab1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
+							: null,
+					backgroundColor: location.pathname === "/" ? null : "#fffde0",
+					backgroundPosition:
+						window.innerWidth > 1200 ? " 50% calc(50% - 300px)" : "center",
+					minHeight: "100vh",
+					backgroundSize: "cover",
+					backgroundRepeat: "no-repeat",
+					width:
+						location.pathname === "/" || location.pathname === "/gallery"
+							? "100vw"
+							: "",
+				}}>
+				<AllRoutes />
+			</div>
+		</>
+	);
 }
